b1fe77bd3475577ecb30670db82ffd910037cf80
Falco Prescher <falcoprescher@googlemail.com>
2018-11-15 18:25:16 +0100

    Basic presentation skeleton

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RxJS-Presentation
 Basics about RxJS
+
+# Source
+[Webpack configuration for reveal.js by Guillaume Calmettes](https://github.com/gcalmettes/reveal.js-webpack-kit)
---
+++ src/_scripts/reveal-dependencies.js
@@ -0,0 +1,21 @@
+// Needed for Reveal
+import 'reveal.js/lib/js/head.min'
+// Cross-browser shim that fully implements classList
+import 'nodePath/reveal.js/lib/js/classList.js'
+// Embed any js visualization (D3, Vega, Vega-lite, Semiotic, etc ...)
+import 'nodePath/reveald3/reveald3.js'
+// Load external files
+import 'nodePath/reveal_external/external/external.js'
+// Handy menu
+import 'nodePath/reveal.js-menu/menu.js'
+
+
+// Speaker notes
+import './reveal-plugins/reveal-notes.js'
+// Modified Math.js to integrate special fragments capabilities in latex equations
+import './reveal-plugins/math-gc.js'
+
+// print slides as pdf if ?print-pdf is appended to the URL
+import './reveal-plugins/print-css.js'
+// Syntax highlighting for code
+import './reveal-plugins/reveal-highlight.js'
